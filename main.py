c26555e0327668f18f572ec6de206f8c76e4170b
mehrnaz <mehrnaz1995@gmail.com>
2021-06-28 22:11:29 -0700

    commented the code

--- main.py
+++ main.py
@@ -4,35 +4,39 @@ import random
 
 
 def find_median(r, s):
+    # make r the longer array
     if len(s) > len(r):
         r_prime = [element for element in r]
         r = [element for element in s]
         s = r_prime
     # len(r)>len(s)
+    # base case
     if len(s) < 3:
-        # log(r) and happens once
+        # log(r) and happens once so does not change the order of the solution
         for element in s:
             r.append(element)
         r.sort()
         return stats.median(r)
     m = len(r)
     n = len(s)
+    # Assumption: we have random access
     r_median = stats.median(r)
     s_median = stats.median(s)
-    # alpha >beta
+
     if r_median == s_median:
         return r_median
-    if m%2 == 0 and n%2 == 0:
+    # This one was a special case that was easier to handle separately
+    if m % 2 == 0 and n % 2 == 0:
         if r_median > s_median:
-            r_prime = r[:int(m - (n / 2)+1)]
-            s_prime = s[int((n / 2)-1):]
+            r_prime = r[:int(m - (n / 2) + 1)]
+            s_prime = s[int((n / 2) - 1):]
             return find_median(r_prime, s_prime)
         else:
-            r_prime = r[int((n / 2)-1):]
-            s_prime = s[: int((n / 2)+1)]
+            r_prime = r[int((n / 2) - 1):]
+            s_prime = s[: int((n / 2) + 1)]
             return find_median(r_prime, s_prime)
+    #This is the general case
     if r_median > s_median:
-        # r is odd & s is odd
         if n % 2 == 1:
             r_prime = r[:int(m - ((n - 1) / 2))]
             s_prime = s[int(((n - 1) / 2)):]
@@ -56,28 +60,23 @@ def find_median(r, s):
 
 
 if __name__ == '__main__':
-    # assumption: r and s are both have len odd
-    # r = [i for i in range(0, 17, 2)]
-    # s = [i for i in range(1, 15, 2)]
+    # the loop is for testing 1000 random cases
     for i in range(1000):
-        m = random.randint(3,20)
-        n = random.randint(3,19)
+        # random length of r and s
+        m = random.randint(3, 20)
+        n = random.randint(3, 19)
+        # filling r and s with random integers
         r = random.sample(range(10, 1000), m)
         s = random.sample(range(10, 1000), n)
         r.sort()
         s.sort()
-        # r = [191, 330, 437, 539, 541, 556, 767, 956]
-        # s =[109, 170, 390, 436, 673, 768, 823, 931]
-        print(r)
-        print(s)
-        # Assumption : n>m
         result = find_median(r, s)
-        print(result)
         concat = np.concatenate((r, s))
         concat.sort()
-        print(concat)
-        print(stats.median(concat))
         print(stats.median(concat) == result)
         if stats.median(concat) != result:
             break
-    print(i)
+    if i < 999:
+        print("failed")
+    else:
+        print("success")
